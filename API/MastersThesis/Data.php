bd75f5109bf8c9a35b11f8741c15215784fd0ccf
Lennart Möller <kontakt@lennartmoeller.com>
2023-11-03 23:51:20 +0100

    Add icons to API

--- API/MastersThesis/Data.php
+++ API/MastersThesis/Data.php
@@ -10,19 +10,37 @@
 
 namespace Apps\MastersThesis\API\MastersThesis;
 
+use Apps\FontAwesome\FontAwesomeException;
+use Apps\FontAwesome\FontAwesomeIcon;
 use Apps\MastersThesis\Model\Account;
 use Apps\MastersThesis\Model\Category;
 use Apps\MastersThesis\Model\Transaction;
 use Core\Base\API;
+use Core\Database\Exceptions\ColumnNotInitializedException;
 use JetBrains\PhpStorm\NoReturn;
 
 class Data extends API {
 
+    /**
+     * @throws ColumnNotInitializedException
+     * @throws FontAwesomeException
+     */
     #[NoReturn] public function get(): void {
+        $categories = Category::get_multiple();
+        $icon_ids = [];
+        foreach ($categories as $category) {
+            $icon_id = $category->icon->get();
+            if ($icon_id === null || in_array($icon_id, $icon_ids, true)) {
+                continue;
+            }
+            $icon_ids[] = $icon_id;
+        }
+        $icons = FontAwesomeIcon::get_multiple(FontAwesomeIcon::STYLE_SOLID, $icon_ids ?? []);
         self::success(array(
             'accounts' => Account::get_multiple(),
-            'categories' => Category::get_multiple(),
-            'transactions' => Transaction::get_multiple()
+            'categories' => $categories,
+            'icons' => $icons,
+            'transactions' => Transaction::get_multiple(),
         ));
     }
 
